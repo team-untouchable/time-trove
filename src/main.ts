367ef2252cd4221fea5becee51562a9657f8aabb
a101201031 <a101201031@gmail.com>
2024-01-14 19:25:21 +0900

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppConfigService } from '@src/config';
 import { AppModule } from './app.module';
@@ -5,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(appConfig.port);
 }
 bootstrap();
