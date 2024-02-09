d394873d161e0a97da13dc2e7ac2becb51716d38
a101201031 <a101201031@gmail.com>
2024-02-09 22:28:20 +0900

    chore: change global provider file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
-import { Module } from '@nestjs/common';
+import type { Provider } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth';
 import { BootstrapModule } from './bootstrap';
 import { UsersModule } from './users';
 import { EventsModule } from './events';
+import { TransformResponseInterceptor } from './common';
+
+const globalProviders: Provider[] = [
+  {
+    provide: APP_PIPE,
+    useValue: new ValidationPipe({
+      transform: true,
+    }),
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: TransformResponseInterceptor,
+  },
+];
 
 @Module({
   imports: [BootstrapModule, AuthModule, UsersModule, EventsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [...globalProviders, AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import type { SwaggerDocumentOptions } from '@nestjs/swagger';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppConfigService } from '@src/config';
 import { AppModule } from './app.module';
-import { TransformResponseInterceptor } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
-
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalInterceptors(new TransformResponseInterceptor());
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle(`${appConfig.name} example`)
